d0db58e8609f5492ba2a5c8662e6b72cd7aacb2e
Richard Buckley <richard.buckley@outlook.com>
2022-12-30 17:27:01 -0500

    File permission class.

---
+++ File/Permissions.cpp
@@ -0,0 +1,12 @@
+//
+// Created by richard on 30/12/22.
+//
+
+/*
+ * Permissions.cpp Created by Richard Buckley (C) 30/12/22
+ */
+
+#include "Permissions.h"
+
+namespace ysh {
+} // ysh
---
+++ File/Permissions.h
@@ -0,0 +1,23 @@
+//
+// Created by richard on 30/12/22.
+//
+
+/*
+ * Permissions.h Created by Richard Buckley (C) 30/12/22
+ */
+
+#ifndef ECOBEEDATA_PERMISSIONS_H
+#define ECOBEEDATA_PERMISSIONS_H
+
+namespace ysh {
+
+/**
+ * @class Permissions
+ */
+    class Permissions {
+
+    };
+
+} // ysh
+
+#endif //ECOBEEDATA_PERMISSIONS_H
