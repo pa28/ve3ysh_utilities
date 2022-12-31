a31dcdeece39872f05e097710acf8a23ff612f9a
Richard Buckley <richard.buckley@outlook.com>
2022-12-30 19:19:59 -0500

    Implement File/Permissions class.

--- File/Permissions.cpp
+++ File/Permissions.cpp
@@ -6,7 +6,46 @@
  * Permissions.cpp Created by Richard Buckley (C) 30/12/22
  */
 
+#include <unistd.h>
+#include <sys/stat.h>
+#include <iostream>
 #include "Permissions.h"
 
 namespace ysh {
+
+    using namespace std::filesystem;
+    void Permissions::evaluate() {
+        fileExists = known = false;
+        procPerms = 0;
+        auto euid = geteuid();
+        auto egid = getegid();
+        if (!filePath.empty()) {
+            if (fileExists = exists(filePath); fileExists) {
+                fileStatus = status(filePath);
+                filePerms = fileStatus.permissions();
+                struct stat statBuf{};
+                if (stat(filePath.c_str(), &statBuf)) {
+                    std::cerr << "Can not stat " << filePath.string() << '\n';
+                } else {
+                    if (euid == statBuf.st_uid) {
+                        procPerms |= ((filePerms & perms::owner_read) != perms::none) ? READ : 0;
+                        procPerms |= ((filePerms & perms::owner_write) != perms::none) ? WRITE : 0;
+                        procPerms |= ((filePerms & perms::owner_exec) != perms::none) ? EXEC : 0;
+                    }
+
+                    if (egid == statBuf.st_gid) {
+                        procPerms |= ((filePerms & perms::group_read) != perms::none) ? READ : 0;
+                        procPerms |= ((filePerms & perms::group_write) != perms::none) ? WRITE : 0;
+                        procPerms |= ((filePerms & perms::group_exec) != perms::none) ? EXEC : 0;
+                    }
+
+                    procPerms |= ((filePerms & perms::others_read) != perms::none) ? READ : 0;
+                    procPerms |= ((filePerms & perms::others_write) != perms::none) ? WRITE : 0;
+                    procPerms |= ((filePerms & perms::others_exec) != perms::none) ? EXEC : 0;
+
+                    known = true;
+                }
+            }
+        }
+    }
 } // ysh
--- File/Permissions.h
+++ File/Permissions.h
@@ -6,6 +6,8 @@
  * Permissions.h Created by Richard Buckley (C) 30/12/22
  */
 
+#include <filesystem>
+
 #ifndef ECOBEEDATA_PERMISSIONS_H
 #define ECOBEEDATA_PERMISSIONS_H
 
@@ -15,7 +17,71 @@ namespace ysh {
  * @class Permissions
  */
     class Permissions {
+    private:
+        std::filesystem::path filePath{};
+        std::filesystem::perms filePerms{};
+        std::filesystem::file_status fileStatus{};
 
+        void evaluate();
+
+        bool fileExists{false}; ///< File exists
+        bool known{false};      ///< File permissions are known
+        unsigned int procPerms{0};  ///< The aggregated permissions the process holds on the file.
+
+    public:
+        static const unsigned int READ{04};
+        static const unsigned int WRITE{02};
+        static const unsigned int EXEC{01};
+
+        Permissions() = default;
+
+        explicit Permissions(std::filesystem::path filePath) : filePath(std::move(filePath)) {
+            evaluate();
+        }
+
+        /**
+         * @brief Test validity of the Permission object.
+         * @return True if the file exists and permissions are known.
+         */
+        explicit operator bool () const { return fileExists && known; }
+
+        /**
+         * @brief Test if the process has read permission on the file.
+         * @return true or false
+         */
+        [[maybe_unused]] [[nodiscard]] bool read() const { return fileExists && known && ((procPerms & READ) != 0); }
+
+        /**
+         * @brief Test if the process has write permission on the file.
+         * @return true of false
+         */
+        [[maybe_unused]] [[nodiscard]] bool write() const { return fileExists && known && ((procPerms & WRITE) != 0); }
+
+        /**
+         * @brief Test if the process has execute permission on the file.
+         * @return true or false
+         */
+        [[maybe_unused]] [[nodiscard]] bool exec() const { return fileExists && known && ((procPerms & EXEC) != 0); }
+
+        /**
+         * @brief Test if the process has all indicated permissions on the file.
+         * @param maskValue a bitwise OR of READ, WRITE, EXEC class constants.
+         * @return true if the process has all requested permissions.
+         */
+        [[maybe_unused]] [[nodiscard]] bool mask(unsigned int maskValue) const {
+            return fileExists && known && ((procPerms & maskValue) == maskValue);
+        }
+
+        /**
+         * @brief Set a new file path and evaluate permissions.
+         * @param path the new file path.
+         * @return true if the file exists and permissions are known.
+         */
+        [[maybe_unused]] bool setFile(std::filesystem::path path) {
+            filePath = std::move(path);
+            evaluate();
+            return fileExists && known;
+        }
     };
 
 } // ysh
