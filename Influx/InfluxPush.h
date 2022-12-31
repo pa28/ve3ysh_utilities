2e4dd54e7dd81db48c80e0dd7f943d6dd7cc205d
Richard Buckley <richard.buckley@outlook.com>
2022-12-31 09:57:37 -0500

    Add convenience function addMeasurement version and documentation.

--- Influx/InfluxPush.h
+++ Influx/InfluxPush.h
@@ -27,7 +27,7 @@
  */
 class InfluxPush {
 private:
-    const std::string TimeFmt{"%Y-%m-%dT%k:%M:%S%z"};
+    const std::string TimeFmt{"%Y-%m-%dT%k:%M:%S%z"};   ///< Timestamp conversion format.
     unsigned long long timeStamp{};
     std::string influxHost{};
     bool connectTls{false};
@@ -42,17 +42,52 @@ public:
     InfluxPush(std::string host, bool tls, long port, std::string dataBase)
         : influxHost(std::move(host)), connectTls(tls), influxPort(port), influxDataBase(std::move(dataBase)) {}
 
+    /**
+     * @brief Set measurement time stamp from a date, time and zone strings. See @refitem TimeFmt.
+     * @param date The data string YYYY-MM-DD.
+     * @param time The time string HH:MM:SS.
+     * @param zone The zone string sHH:MM offset from GMT.
+     */
     void setMeasurementEpoch(const std::string& date, const std::string& time, const std::string& zone);
 
+    /**
+     * @brief Get the current time stamp in nanoseconds from epoch.
+     */
+    [[maybe_unused]] [[nodiscard]] auto getMeasurementEpoch() const { return timeStamp; }
+
+    /**
+     * @brief Clear the measurements store.
+     */
     void newMeasurements() {
         measurements.str("");
     }
 
+    /**
+     * @brief Add a measurement to the measurement store using the internal time stamp.
+     * @param prefix The measurement prefix.
+     * @param name The measurement value name.
+     * @param value The measurement value.
+     */
     void addMeasurement(const std::string& prefix, const std::optional<std::string>& name, const std::optional<std::string>& value) {
-        if (name.has_value() && !name.value().empty() && value.has_value() && !value.value().empty())
-            measurements << prefix << name.value() << '=' << value.value() << ' ' << timeStamp << '\n';
+        addMeasurement(prefix, name, value, timeStamp);
     }
 
+    /**
+     * @brief Add a measurement to the measurement store using a specified time stamp in nanoseconds from epoch.
+     * @param prefix The measurement prefix.
+     * @param name The measurement value name.
+     * @param value The measurement value.
+     */
+    void addMeasurement(const std::string& prefix, const std::optional<std::string>& name,
+                        const std::optional<std::string>& value, unsigned long long useTimeStamp) {
+        if (name.has_value() && !name.value().empty() && value.has_value() && !value.value().empty())
+            measurements << prefix << name.value() << '=' << value.value() << ' ' << useTimeStamp << '\n';
+    }
+
+    /**
+     * @brief Push the measurement store to the InfluxDB.
+     * @return true if successful.
+     */
     bool pushData();
 
     [[maybe_unused]] auto getMeasurements() const {
