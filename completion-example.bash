8237f66b4ccaa720fc1aaf05c041a9bcf2c0efb2
Richard Buckley <richard.buckley@outlook.com>
2023-02-09 21:10:33 -0500

    Prototype command line completion script.

---
+++ completion-example.bash
@@ -0,0 +1,21 @@
+#/usr/bin/env bash
+_BetterMain_completions()
+{
+    local file_arg=("-i -o -c --input --output --config");
+    local args=("-v --verbose");
+    local prev_arg;
+    local curr_arg;
+
+    if [[ ${#COMP_WORDS[@]} -ge 2 ]]; then
+        prev_arg="${COMP_WORDS[COMP_CWORD-1]}"
+        curr_arg="${COMP_WORDS[COMP_CWORD]}"
+
+	if [[ " ${file_arg[*]} " =~ " ${prev_arg} " ]]; then
+	    COMPREPLY=($(compgen -f -- "${curr_arg}"))
+        else
+           COMPREPLY=($(compgen -W "${file_arg[*]} ${args[*]}" -- "${curr_arg}"))
+	fi
+    fi
+}
+
+complete -o filenames -F _BetterMain_completions BetterMain
